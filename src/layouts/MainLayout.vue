d6029010f23e219e71f51c95c9731f8b42d80743
Peter <abc1870819884@gmail.com>
2023-08-21 10:10:49 -0700

    change svg server

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,10 +1,16 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header class="header" flat :style="{background:headerBgColor}">
+    <q-header class="header" flat :style="{ background: headerBgColor }">
       <q-toolbar>
-        <q-btn round flat @click="goToSite"><q-icon name="img:/home.svg" /></q-btn>
-        <q-toolbar-title class="text-center" style="font-size:1.1em"> Step by step の Diary </q-toolbar-title>
-        <q-btn @click="toggleTheme" round flat><q-icon :name="`img:/${mode}.svg`" /></q-btn>
+        <q-btn round flat @click="goToSite"
+          ><q-icon name="img:/home.svg"
+        /></q-btn>
+        <q-toolbar-title class="text-center" style="font-size: 1.1em">
+          Step by step の Diary
+        </q-toolbar-title>
+        <q-btn @click="toggleTheme" round flat
+          ><q-icon :name="`img:https://www.stepbystep.cf/assets/${mode}.svg`"
+        /></q-btn>
       </q-toolbar>
     </q-header>
     <q-page-container>
@@ -14,22 +20,21 @@
 </template>
 
 <script setup lang="ts">
-import { useQuasar } from 'quasar'
+import { useQuasar } from 'quasar';
 import { ref } from 'vue';
-const $q = useQuasar()
-const headerBgColor= ref('#fff');
-const mode= ref('moon');
-const goToSite = ()=> {
-      window.location.href = 'https://www.stepbystep.cf/';
-    }
-    const toggleTheme=()=> {
-      $q.dark.toggle();
-      headerBgColor.value=$q.dark.isActive ? '#292828':'#fff';
-      mode.value=$q.dark.isActive ? 'sun':'moon';
-    }
+const $q = useQuasar();
+const headerBgColor = ref('#fff');
+const mode = ref('moon');
+const goToSite = () => {
+  window.location.href = 'https://www.stepbystep.cf/';
+};
+const toggleTheme = () => {
+  $q.dark.toggle();
+  headerBgColor.value = $q.dark.isActive ? '#292828' : '#fff';
+  mode.value = $q.dark.isActive ? 'sun' : 'moon';
+};
 </script>
 <style scoped lang="scss">
-
 header {
   height: 5rem;
   line-height: 5rem;
