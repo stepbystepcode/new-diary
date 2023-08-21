6f57aa2a78e6c080e46a3b7c745dc2d568763aef
Peter <abc1870819884@gmail.com>
2023-08-21 09:59:04 -0700

    config vercel 2

--- quasar.config.js
+++ quasar.config.js
@@ -64,7 +64,7 @@ module.exports = configure(function (/* ctx */) {
       // publicPath: '/',
       // analyze: true,
       env: require('dotenv').config().parsed,
-      distDir: ctx.mode.spa ? 'public' : null,
+      distDir: 'public',
       // rawDefine: {}
       // ignorePublicFolder: true,
       // minify: false,
